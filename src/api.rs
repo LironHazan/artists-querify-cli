8f9060084997b91aa34d36ce987f10d140ede77c
lironh <lironenk@gmail.com>
2020-09-05 10:31:09 +0300

    feat(api): removed comment

--- src/api.rs
+++ src/api.rs
@@ -4,13 +4,8 @@ use crate::artists::{ArtistInfo};
 pub async fn search(name: String) -> Result<ArtistInfo, ExitFailure> {
     let mut url = format!("https://www.theaudiodb.com/api/v1/json/1/search.php?s=");
     url = [url, name].join("\n");
-    let result = reqwest::get(&url).await?
+    let res = reqwest::get(&url).await?
         .json::<ArtistInfo>()
         .await?;
-    Ok(result)
-
-    // match make_request() {
-    //     Err(e) => handler(e),
-    //     Ok(_)  => return,
-   // }
+    Ok(res)
 }
